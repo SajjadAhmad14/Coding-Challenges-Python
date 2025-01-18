85b86929351e1152e2834deddb5035622c68da31
Sajjad Ahmad <jogimar14@gmail.com>
2025-01-18 19:44:13 +0500

    Add method to count words in a file

--- wc-tool/ccwc.py
+++ wc-tool/ccwc.py
@@ -10,11 +10,14 @@ def main():
   option = sys.argv[1]
   file = sys.argv[2]
   if option == "-c":
-    word_count(option, file)
+    count_bytes(option, file)
   if option == "-l":
     count_lines(option, file)
+  if option == "-w":
+    count_words(option, file)
 
-def word_count(o, f):
+
+def count_bytes(o, f):
   file_path = f
   with open(file_path, 'rb') as f:
     file_content = f.read()
@@ -29,6 +32,14 @@ def count_lines(o, f):
       count = count + 1
   print(count, file_path)
 
+def count_words(o, f):
+  file_path = f
+  with open(file_path, "r") as f:
+    word_count = 0
+    for line in f:
+      words = line.split()
+      word_count += len(words)
+    print(word_count, file_path)
 
 if __name__=="__main__":
   main()
