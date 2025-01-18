e73b0df588bdc6e764ec55c99a6ddcd3b4d85b0c
Sajjad Ahmad <jogimar14@gmail.com>
2025-01-18 19:30:38 +0500

    Create our own wc tool

---
+++ wc-tool/ccwc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+import os
+import chardet
+
+def main():
+  if len(sys.argv) < 3 or len(sys.argv) > 3:
+    sys.exit()
+
+  option = sys.argv[1]
+  file = sys.argv[2]
+  if option == "-c":
+    word_count(option, file)
+  if option == "-l":
+    count_lines(option, file)
+
+def word_count(o, f):
+  file_path = f
+  with open(file_path, 'rb') as f:
+    file_content = f.read()
+    file_size_bytes = len(file_content)
+  print(file_size_bytes, file_path)
+
+def count_lines(o, f):
+  file_path = f
+  count = 0
+  with open(file_path, "r") as f:
+    for _ in f:
+      count = count + 1
+  print(count, file_path)
+
+
+if __name__=="__main__":
+  main()
---
+++ wc-tool/instructions.md
@@ -0,0 +1,17 @@
+# steps to run the script
+
+# 1. Add a Shebang to Your Script
+
+#!/usr/bin/env python3
+
+# 2. Make the File Executable
+
+# chmod +x ccwc.py
+
+# 3. Create a symlink to your script:
+
+# sudo ln -s /path/to/ccwc.py /usr/local/bin/ccwc
+
+# 4. Move ccwc to a PATH Directory
+
+# sudo mv ccwc /usr/local/bin/ccwc
