145f3f4ab7ed2ca7005d749df22df4e5329e009b
Sajjad Ahmad <jogimar14@gmail.com>
2025-01-20 23:45:03 +0500

    Add method for charcters count and refator code

--- wc-tool/ccwc.py
+++ wc-tool/ccwc.py
@@ -4,42 +4,65 @@ import os
 import chardet
 
 def main():
-  if len(sys.argv) < 3 or len(sys.argv) > 3:
+  if len(sys.argv) < 2 or len(sys.argv) > 3:
     sys.exit()
+  if len(sys.argv) == 3:
+    option = sys.argv[1]
+    file = sys.argv[2]
+    if option == "-c":
+      bytes = count_bytes(file)
+      print(bytes, file)
+    elif option == "-l":
+      lines = count_lines(file)
+      print(lines, file)
+    elif option == "-w":
+      words = count_words(file)
+      print(words, file)
+    elif option == "-m":
+      chars = count_chars(file)
+      print(chars, file)
+    else:
+      print("no")
+  else:
+    file = sys.argv[1]
+    handle_default_option(file)
 
-  option = sys.argv[1]
-  file = sys.argv[2]
-  if option == "-c":
-    count_bytes(option, file)
-  if option == "-l":
-    count_lines(option, file)
-  if option == "-w":
-    count_words(option, file)
 
 
-def count_bytes(o, f):
+
+def count_bytes(f):
   file_path = f
   with open(file_path, 'rb') as f:
     file_content = f.read()
     file_size_bytes = len(file_content)
-  print(file_size_bytes, file_path)
+  return file_size_bytes
 
-def count_lines(o, f):
+def count_lines(f):
   file_path = f
-  count = 0
+  lines = 0
   with open(file_path, "r") as f:
     for _ in f:
-      count = count + 1
-  print(count, file_path)
+      lines = lines + 1
+  return lines
 
-def count_words(o, f):
+def count_words(f):
   file_path = f
   with open(file_path, "r") as f:
     word_count = 0
     for line in f:
       words = line.split()
       word_count += len(words)
-    print(word_count, file_path)
+    return word_count
+
+def count_chars(f):
+  file_path = f
+  with open(file_path, "r") as f:
+    content = f.read()
+    newline_count = content.count('\n')
+  return len(content) + newline_count
+
+def handle_default_option(f):
+  print()
 
 if __name__=="__main__":
   main()
