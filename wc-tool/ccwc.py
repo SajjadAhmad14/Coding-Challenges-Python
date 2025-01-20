bd21632ef3585495de2d0ecfc2fbc804b4d07bb7
Sajjad Ahmad <jogimar14@gmail.com>
2025-01-21 02:01:13 +0500

    Add logic to catter standard input from file

--- wc-tool/ccwc.py
+++ wc-tool/ccwc.py
@@ -1,68 +1,64 @@
 #!/usr/bin/env python3
 import sys
-import os
-import chardet
 
 def main():
-  if len(sys.argv) < 2 or len(sys.argv) > 3:
-    sys.exit()
-  if len(sys.argv) == 3:
-    option = sys.argv[1]
-    file = sys.argv[2]
-    if option == "-c":
-      bytes = count_bytes(file)
-      print(bytes, file)
-    elif option == "-l":
-      lines = count_lines(file)
-      print(lines, file)
-    elif option == "-w":
-      words = count_words(file)
-      print(words, file)
-    elif option == "-m":
-      chars = count_chars(file)
-      print(chars, file)
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        sys.exit("Invalid input")
+
+    if len(sys.argv) == 3:
+        option = sys.argv[1]
+        file = sys.argv[2]
+        if option == "-c":
+            bytes = count_bytes(file)
+            print(bytes, file)
+        elif option == "-l":
+            lines = count_lines(file)
+            print(lines, file)
+        elif option == "-w":
+            words = count_words(file)
+            print(words, file)
+        elif option == "-m":
+            chars = count_chars(file)
+            print(chars, file)
+        else:
+            print("Invalid option")
     else:
-      print("no")
-  else:
-    file = sys.argv[1]
-    handle_default_option(file)
-
-
-
+        if sys.stdin.isatty():
+            file = sys.argv[1]
+            lines = count_lines(file)
+            words = count_words(file)
+            bytes = count_bytes(file)
+            print(lines, words, bytes, file)
+        else:
+          option = sys.argv[1]
+          content = sys.stdin.read()
+          if option == "-l":
+            print(content.count('\n'))
+          elif option == "-w":
+            print(len(content.split()))
+          elif option == "-c":
+            print(len(content.encode()))
+          elif option == "-m":
+            print(len(content) + content.count('\n'))
+          else:
+            sys.exit("Invalid option.")
 
 def count_bytes(f):
-  file_path = f
-  with open(file_path, 'rb') as f:
-    file_content = f.read()
-    file_size_bytes = len(file_content)
-  return file_size_bytes
+  with open(f, 'rb') as file:
+    return len(file.read())
 
 def count_lines(f):
-  file_path = f
-  lines = 0
-  with open(file_path, "r") as f:
-    for _ in f:
-      lines = lines + 1
-  return lines
+  with open(f, 'r') as file:
+    return sum(1 for _ in file)
 
 def count_words(f):
-  file_path = f
-  with open(file_path, "r") as f:
-    word_count = 0
-    for line in f:
-      words = line.split()
-      word_count += len(words)
-    return word_count
+  with open(f, 'r') as file:
+    return sum(len(line.split()) for line in file)
 
 def count_chars(f):
-  file_path = f
-  with open(file_path, "r") as f:
-    content = f.read()
-    newline_count = content.count('\n')
-  return len(content) + newline_count
+  with open(f, 'r') as file:
+    content = file.read()
+    return len(content) + content.count('\n')
 
-def handle_default_option(f):
-  print()
-
-if __name__=="__main__":
-  main()
+if __name__ == "__main__":
+    main()
